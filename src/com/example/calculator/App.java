542917adab88b69877de4541dc4edd1ad3ef6c25
Kang Dal Ho <d4lh0w@naver.com>
2025-03-05 14:08:24 +0900

    계산기 만들기

---
+++ src/com/example/calculator/App.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            double num1 = sc.nextDouble();
+
+            System.out.println("사칙연산 기호를 입력하세요(+,-,*,/): ");
+            char op = sc.next().charAt(0);
+
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            double num2 = sc.nextDouble();
+
+            double result = calculator.calculate(num1, num2, op);
+            System.out.println("값 = " + result);
+
+            System.out.println("현재 연산 기록:" + calculator.getHistory());
+
+            System.out.println("연산 기록을 삭제하시겠습니까?(yes 입력시 삭제)");
+            String deleteAnswer = sc.next();
+            if (deleteAnswer.equals("yes")) {
+                calculator.removeResult();
+            }
+
+            System.out.println("더 계산하시겠습니까?(exit 입력시 종료)");
+            String answer2 = sc.next();
+            if (answer2.equals("exit"))
+                break;
+        }
+    }
+}
---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,52 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    //속성
+    private final List<Double> history;
+
+    //생성자
+    public Calculator() {
+        this.history = new ArrayList<>();
+    }
+
+    //메서드
+    public double calculate(double num1, double num2, char op) {
+        double result;
+        if (op == '+') {
+            result = num1 + num2;
+        } else if (op == '-') {
+            result = num1 - num2;
+        } else if (op == '*') {
+            result = num1 * num2;
+        } else if (op == '/') {
+            if (num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                return Double.NaN;
+            }
+            result = num1 / num2;
+        } else {
+            System.out.println("잘못된 연산자입니다.");
+            return Double.NaN;
+        }
+        //연산 결과 저장
+        history.add(result);
+        return result;
+    }
+    //getter 메서드
+    public List<Double> getHistory() {
+        return new ArrayList<>(history);
+    }
+    //삭제 메서드
+    public void removeResult() {
+        if (!history.isEmpty()) {
+            history.remove(0);
+            System.out.println("가장 오래된 연산 기록이 삭제되었습니다.");
+        } else {
+            System.out.println("삭제할 연산 기록이 없습니다.");
+        }
+    }
+}
